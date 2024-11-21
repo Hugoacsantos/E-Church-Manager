10ed8a18bc202e59894da4fda3f9baa9d248fbda
Hugo Santos <hugoantonio.netto@gmail.com>
2024-11-21 17:06:23 -0300

    refactor: adicao do refresh do banco de dados no test

--- tests/Feature/AddressTest.php
+++ tests/Feature/AddressTest.php
@@ -1,20 +1,25 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
 use App\Models\User;
 
 test('Deve criar um novo endereço', function () {
 
-    $data = [
-        'name' => fake()->name(),
-        'email' => fake()->email(),
-        'password' => fake()->regexify('[A-Z]{5}[0-4]{3}')
-    ];
+    $user = User::factory()->create();
+    // $data = [
+    //     'name' => fake()->name(),
+    //     'email' => fake()->email(),
+    //     'password' => fake()->regexify('[A-Z]{5}[0-4]{3}')
+    // ];
 
-    $response1 = $this->postJson('/api/user/create',$data);
-    $user = $response1->getData();
+    // $response1 = $this->postJson('/api/user/create',$data);
+    // $user = $response1->getData();
 
     $data2 = [
-        'user_id' => $user->id, // Um ID de usuário válido
+        'user_id' => $user->id,
         'rua' => 'Rua Teste',
         'numero' => '123',
         'complemento' => 'Apto 456',
@@ -138,4 +143,4 @@ test('Deve retornar um ou mais endereço', function() {
 
     expect($response->Json())->not()->toBeEmpty();
     $response->assertStatus(200);
-})->only();
+});
