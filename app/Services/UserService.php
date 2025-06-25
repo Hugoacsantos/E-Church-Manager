c5835039b301d85647ba57e88290dc03a43bf1e5
Hugo Santos <hugoantonio.netto@gmail.com>
2025-06-25 20:36:27 -0300

    fix: correcao no momento de salvar o email

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -20,7 +20,7 @@ class UserService {
         }
         $user = new User();
         $user->name = $userDTO->name;
-        $user->email = $userDTO->name;
+        $user->email = $userDTO->email;
         $user->password = $userDTO->password;
         $user->save();
 
