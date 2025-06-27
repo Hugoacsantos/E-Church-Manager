1842bc9f90e9f627959c1d352e090af588e57244
Hugo Santos <hugoantonio.netto@gmail.com>
2025-06-27 19:33:14 -0300

    fix: colocando a injecao de dependencia direto no metodo invoke

--- app/Http/Controllers/Address/FindAddressController.php
+++ app/Http/Controllers/Address/FindAddressController.php
@@ -9,16 +9,11 @@ use Illuminate\Http\Request;
 
 class FindAddressController extends Controller
 {
-    public function __construct(
-        public AddressService $addressService
-    ) {
-
-    }
     /**
      * Handle the incoming request.
      */
-    public function __invoke(string $id) {
-        $endereco = $this->addressService->findById($id);
+    public function __invoke(string $id, AddressService $addressService) {
+        $endereco = $addressService->findById($id);
         return \response()->json($endereco);
     }
 }
